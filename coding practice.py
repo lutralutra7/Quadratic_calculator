e41b2cb67b05ad2efe77af7849d4bdffb531b653
lutralutra7 <123402124+lutralutra7@users.noreply.github.com>
2023-01-24 03:13:28 -0800

    Update coding practice.py

--- coding practice.py
+++ coding practice.py
@@ -1,6 +1,7 @@
-name = input('Enter name')
-if name ==('dan'):
- print('Welcome')
-else:
- print('good bye.')
+import cmath as np
+a, b, c = (input('Enter coefficient a: ')), (input('Enter coefficient b: ')), (input('Enter coefficient c: '))
+a, b, c = complex(a), complex(b), complex(c)
+ans, ans2 = (-b-np.sqrt(b*b-4*a*c))/(2*a), (-b+np.sqrt(b*b-4*a*c))/(2*a)
+print(ans), print(ans2)
+
 
